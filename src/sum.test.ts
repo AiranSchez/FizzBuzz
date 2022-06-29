e21fc838919d6dccf0ea3b77591fd0c94f7571d7
Airan <airanschez0@gmail.com>
2022-06-29 19:43:32 +0100

    :white_check_mark: add tests

--- src/sum.test.ts
+++ src/sum.test.ts
@@ -1,5 +1,24 @@
-import sum from ".";
+import fizzbuzz from ".";
 
-test('Check sum function ', () => {
-    expect(sum(1, 2)).toBe(3)
+describe('FizzBuzz should', () => {
+    test('return fizz when number is multiple of 3', () => {
+        expect(fizzbuzz(3)).toBe('fizz')
+        expect(fizzbuzz(6)).toBe('fizz')
+        expect(fizzbuzz(9)).toBe('fizz')
+    })
+    test('return buzz when number is multiple of 5', () => {
+        expect(fizzbuzz(5)).toBe('buzz')
+        expect(fizzbuzz(10)).toBe('buzz')
+    })
+    test('return fizzbuzz when number is multiple of 3 and 5', () => {
+        expect(fizzbuzz(15)).toBe('fizzbuzz')
+        expect(fizzbuzz(30)).toBe('fizzbuzz')
+    })
+    test('return fizz when number is multiple of 3', () => {
+        expect(fizzbuzz(-3)).toBe('fizz')
+    })
+    test('return fizz when number is multiple of 3', () => {
+        expect(fizzbuzz(1)).toBe('1')
+        expect(fizzbuzz(7)).toBe('7')
+    })
 });
