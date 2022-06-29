7ef5c03afa2b07aa82292d148f65d9e9f2d986f2
Airan <airanschez0@gmail.com>
2022-06-29 19:44:21 +0100

    :sparkles: add fizzbuzz kata first iteration

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
-const sum = (num1: number, num2: number): number => {
-    return num1 + num2;
+const sum = (num1: number): string => {
+    const isMultipleOfThree = num1 % 3 == 0
+    const isMultipleOfFive = num1 % 5 == 0
+
+    return (isMultipleOfThree && isMultipleOfFive) ? "fizzbuzz"
+        : isMultipleOfFive ? "buzz"
+        : isMultipleOfThree ? "fizz"
+        : num1.toString()
 }
 export default sum
